c23df41cd96073b3be4c0455c1ff8109a9049032
Shayne Boyer <shayne.p.boyer@disney.com>
2016-04-20 09:01:15 -0400

    change from gulp.ts to js

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+"use strict";
+var gulp = require("gulp");
+var del = require("del");
+var sourcemaps = require('gulp-sourcemaps');
+
+/**
+ * Remove build directory.
+ */
+gulp.task('clean', function (cb) {
+    return del(["build"], cb);
+});
+
+/**
+ * Copy all resources that are not TypeScript files into build directory.
+ */
+gulp.task("resources", ["server"], function () {
+    return gulp.src(["src/**/*", "!**/*.ts", "!src/server", "!src/server/**"])
+        .pipe(gulp.dest("build"));
+});
+gulp.task("server", function () {
+    return gulp.src(["index.js", "package.json"], { cwd: "src/server/**" })
+        .pipe(gulp.dest("build"));
+});
+/**
+ * Copy all required libraries into build directory.
+ */
+gulp.task("libs", function () {
+    return gulp.src([
+        'es6-shim/es6-shim.min.js',
+        'systemjs/dist/system-polyfills.js',
+        'angular2/bundles/angular2-polyfills.js',
+        'angular2/es6/dev/src/testing/shims_for_IE.js',
+        'systemjs/dist/system.src.js',
+        'rxjs/bundles/Rx.js',
+        'angular2/bundles/angular2.dev.js',
+        'angular2/bundles/router.dev.js'
+    ], { cwd: "node_modules/**" }) /* Glob required here. */
+        .pipe(gulp.dest("build/lib"));
+});
+/**
+ * Build the project.
+ */
+gulp.task("default", ['resources', 'libs'], function () {
+    console.log("Building the project ...");
+});
